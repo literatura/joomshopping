9430742dca4ec164cae63591d632967998ce5c14
literatura <niyaz.musin@yandex.ru>
2016-02-10 23:21:35 +0500

    Fix problems in backend

--- addons/jdownloads_files/source/plugins/jshoppingadmin/jdownloads_files_for_product/jdownloads_files_for_product.php
+++ addons/jdownloads_files/source/plugins/jshoppingadmin/jdownloads_files_for_product/jdownloads_files_for_product.php
@@ -3,42 +3,64 @@ defined('_JEXEC') or die('Restricted access');
 
 class plgJshoppingAdminjdownloads_files_for_product extends JPlugin
 {
-	var $separator = "{jdownloads_cat_id}";
+	var $sep1 = "{jdownloads_cat_id}";
+	var $sep2 = "{/jdownloads_cat_id}";
 	
 	function __construct(&$subject, $config){
 		parent::__construct($subject, $config);
+
 	}
 
 	function onBeforeDisplaySaveProduct(&$post, &$product) {
 		$_lang = JModelLegacy::getInstance('Languages','JshoppingModel');
 		$languages = $_lang->getAllLanguages(1);
         foreach($languages as $lang){
-            $post['description_'.$lang->language] .= $this->separator . JRequest::getVar('jdownloads_cat_id_'.$lang->id,'','post',"string", 2);
+            //$post['description_'.$lang->language] .= $this->sep1 . JRequest::getVar('jdownloads_cat_id_'.$lang->id,'','post',"string", 2) . $this->sep2;
+            $post['description_'.$lang->language] = $this->sep1 . JRequest::getVar('jdownloads_cat_id_'.$lang->id,'','post',"string", 2) . $this->sep2 . $post['description_'.$lang->language];
         }
+
 	}
 	
 	function onBeforeDisplayEditProduct(&$product, &$related_products, &$lists, &$listfreeattributes, &$tax_value) {
+		//die("123");
 		$_lang = JModelLegacy::getInstance('Languages','JshoppingModel');
 		$languages = $_lang->getAllLanguages(1);
         foreach($languages as $lang){
-			$tmp = explode($this->separator, $product->{'description_'.$lang->language});
+			/*$tmp = explode($this->sep, $product->{'description_'.$lang->language});
 			if (isset($tmp[1])) {
 				$product->{'description_'.$lang->language} = $tmp[0];
 				$product->{'jdownloads_cat_id_'.$lang->language} = $tmp[1];
 			} else {
 				$product->{'jdownloads_cat_id_'.$lang->language} = '';
+			}*/
+
+			$start = mb_strpos($product->{'description_'.$lang->language}, $this->sep1);
+			$end = mb_strpos($product->{'description_'.$lang->language}, $this->sep2);
+
+			if($end!=false){				
+				$product->{'jdownloads_cat_id_'.$lang->language} = mb_substr($product->{'description_'.$lang->language}, ($start+mb_strlen($this->sep1)), ($end-$start-mb_strlen($this->sep1)));
+				$product->{'description_'.$lang->language} = mb_substr($product->{'description_'.$lang->language}, ($end+mb_strlen($this->sep2)));
+				//echo $product->{'description_'.$lang->language}; die();
+			}else{
+				$product->{'jdownloads_cat_id_'.$lang->language} = '';
 			}
+
+			
+
         }
 	}
 	
 	function onBeforeDisplayEditProductView(&$view) {
+		//die("567");
 		$_lang = JModelLegacy::getInstance('Languages','JshoppingModel');
 		$languages = $_lang->getAllLanguages(1);
         foreach($languages as $lang){
 			if (isset($view->product->{'jdownloads_cat_id_'.$lang->language})) {
 				if (!isset($view->{'plugin_template_description_'.$lang->language})) $view->{'plugin_template_description_'.$lang->language} = '';
-				$second_description = "jdownloads_cat_id".$lang->language;
-				$view->{'plugin_template_description_'.$lang->language} .= '<tr><td class="key">ID категории jDownloads</td><td><input type="text" name="jdownloads_cat_id_'.$lang->id.'" /></td></tr>';
+				$jdownloads_cat_id = "jdownloads_cat_id_".$lang->language;
+				$view->{'plugin_template_description_'.$lang->language} .= '<tr><td class="key">ID категории jDownloads</td><td><input type="text" name="jdownloads_cat_id_'.$lang->id.'" value="'.$view->product->$jdownloads_cat_id.'" /></td></tr>';
+
+				/*$editor->display('second_description'.$lang->id,  $view->product->$jdownloads_cat_id , '100%', '350', '75', '20' )*/
 			}
 		}
 	}
--- addons/jdownloads_files/source/plugins/jshoppingproducts/jdownloads_files_for_product/jdownloads_files_for_product.php
+++ addons/jdownloads_files/source/plugins/jshoppingproducts/jdownloads_files_for_product/jdownloads_files_for_product.php
@@ -3,7 +3,8 @@ defined('_JEXEC') or die('Restricted access');
 
 class plgJshoppingProductsjdownloads_files_for_product extends JPlugin
 {
-	var $separator = "{jdownloads_cat_id}";
+	var $sep1 = "{jdownloads_cat_id}";
+	var $sep2 = "{/jdownloads_cat_id}";
 	
 	function __construct(&$subject, $config){
 		parent::__construct($subject, $config);
@@ -11,14 +12,26 @@ class plgJshoppingProductsjdownloads_files_for_product extends JPlugin
     
 	function onBeforeDisplayProduct(&$product, &$view, &$product_images, &$product_videos, &$product_demofiles) {
 		$lang = JSFactory::getLang();
-        $description = $lang->get('description');
-		$second_description = 'second_'.$description;
-		$tmp = explode($this->separator, $product->$description);
-		if (isset($tmp[1])) {
-			$product->description = $tmp[0];
-			$product->second_description = $tmp[1];
-			if (!isset($view->_tmp_product_html_after_buttons)) $view->_tmp_product_html_after_buttons = '';
-			$view->_tmp_product_html_after_buttons .= '<div class="second_description">'.$product->second_description.'</div>';
+
+		$start = mb_strpos($product->description, $this->sep1);
+		$end = mb_strpos($product->description, $this->sep2);
+
+		$product->jdownloads_cat_id = 0;
+
+		if($end!=false){				
+			$product->jdownloads_cat_id = mb_substr($product->description, ($start+mb_strlen($this->sep1)), ($end-$start-mb_strlen($this->sep1)));
+			$product->description = mb_substr($product->description, ($end+mb_strlen($this->sep2)));
+
+			if (!isset($view->_tmp_product_html_jdownloads)) $view->_tmp_product_html_jdownloads = '';
+			if($product->jdownloads_cat_id != 0){
+				jimport( 'joomla.application.module.helper' );
+				$module = JModuleHelper::getModule( 'mod_jdownloads_category' );
+				$params = new JRegistry();
+				$params->loadString($module->params);				
+				$params->set('cat_id', $product->jdownloads_cat_id);
+				$module->params = $params->toString();
+				$view->_tmp_product_html_jdownloads = JModuleHelper::renderModule( $module );
+			}
 		}
 	}
 	
